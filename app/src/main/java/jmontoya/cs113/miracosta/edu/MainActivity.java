25a09f2f5d6f1c3672d5705076cbd825638ad1f4
Jay Montoya <jayjoplin9898@gmail.com>
2019-01-24 15:58:58 -0800

    I inserted one comment!

--- app/src/main/java/jmontoya/cs113/miracosta/edu/MainActivity.java
+++ app/src/main/java/jmontoya/cs113/miracosta/edu/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // this is a change
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
